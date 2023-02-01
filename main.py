4a9d02903ba2a078a4a9d4ca365d6bf19f286ce0
zhongmatthew <s10243543@connect.np.sg>
2023-02-01 23:11:38 +0800

    Added comments for main file

--- main.py
+++ main.py
@@ -1,12 +1,30 @@
+# import other files as modules
 import coh, overheads, profit_loss
 
+# use "def" to create a main function to modularize the programn
 def main():
     """
     - Function calls and execute each function from different files
     """
-
+    
+    # from each module, orderly call and execute each function 
     overheads.overhead()
     coh.coh()
     profit_loss.profitloss()
 
+# call "main" function and execute
 main()
+
+# ----------------- Base solution ----------------- #
+# import coh, overheads, profit_loss
+
+# def main():
+#     """
+#     - Function calls and execute each function from different files
+#     """
+
+#     overheads.overhead()
+#     coh.coh()
+#     profit_loss.profitloss()
+
+# main()
