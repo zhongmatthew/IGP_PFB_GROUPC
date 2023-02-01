fc027fee5a13a1adac8fa26f2bb19b47758f6291
zhongmatthew <s10243543@connect.np.sg>
2023-02-01 14:06:03 +0800

    edit overhead code

--- overheads.py
+++ overheads.py
@@ -1,8 +1,12 @@
-def overheads():
+def overhead():
+    """
+    - Function will determine the highest overhead
+    """
     from pathlib import Path
     import csv
     
     fp = Path.cwd()/"csv_reports\overheads.csv"
+
     with fp.open(mode="r", encoding="UTF-8", newline="") as file:
         reader = csv.reader(file)
         next(reader)
@@ -19,7 +23,7 @@ def overheads():
 
     for heading, percentage in overheads:
         if float(percentage) == max(percentage_list):
-            with open("Summary_Report.txt", mode = "a", encoding="UTF-8") as file:
-                file.write(f"[HIGHEST OVERHEADS] {headings}: {percentage}%\n")
+            with open("Summary_Report.txt", mode = "w", encoding="UTF-8") as file:
+                file.write(f"[HIGHEST OVERHEADS] {heading}: {percentage}%\n")
 
-overheads()
+overhead()
