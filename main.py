6312ab25d9038f2e4f78bca2d14bbfd3b2f55f6c
zhongmatthew <s10243543@connect.np.sg>
2023-02-01 17:36:23 +0800

    Modularized the code

---
+++ main.py
@@ -0,0 +1,12 @@
+import coh, overheads, profit_loss
+
+def main():
+    """
+    - Function calls and execute each function from different files
+    """
+
+    overheads.overhead()
+    coh.coh()
+    profit_loss.profitloss()
+
+main()
